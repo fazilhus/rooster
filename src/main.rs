3469c602af649917b3054a4e2ce9c124fdf9e98c
fazilhus <khusnutdinov.fazil@gmail.com>
2023-12-08 19:33:15 +0100

    parsing a directory instead of one file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use std::io;
-use std::fs::File;
-use std::process::exit;
+use std::{fs, io};
+use std::path::Path;
 use xml::reader::{XmlEvent, EventReader};
 
-fn xml_to_string(file_path: &str) -> io::Result<String> {
-    let file = File::open(file_path)?;
+fn xml_to_string<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = fs::File::open(file_path)?;
     let event_reader = EventReader::new(file);
 
     let mut content = String::new();
     for event in event_reader.into_iter() {
         if let XmlEvent::Characters(text) = event.expect("TODO") {
             content.push_str(&text);
+            content.push(' ');
         }
     }
     Ok(content)
 }
+
 fn main() {
-    let file_path = "../docs.gl/gl4/glClear.xhtml";
-    let content = xml_to_string(file_path).expect("TODO");
-    println!("{content}");
+    let dir_path = "../docs.gl/gl4";
+    let dir = fs::read_dir(dir_path).unwrap();
+    for entry in dir {
+        let file_path = entry.unwrap().path();
+        let content = xml_to_string(&file_path).unwrap();
+        println!("{file_path:?} -> {}", content.len());
+    }
 }
